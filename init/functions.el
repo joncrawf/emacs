233f6b855e248b2bf1ffa95655a418a96864daca
Jon Crawford <jonacrawf@gmail.com>
2016-06-14 23:33:07 +0100

    Fixed duplicate line function

--- init/functions.el
+++ init/functions.el
@@ -4,20 +4,20 @@
 ;;; Code:
 
 (defun comment-it ()
-  "comment or uncomment region or line"
+  "Comment or uncomment region or line."
   (interactive)
   (if mark-active
     (comment-or-uncomment-region (region-beginning) (region-end))
     (comment-or-uncomment-region (line-beginning-position) (line-end-position))))
 
 (defun duplicate-line ()
-	"duplicate line under mark"
+	"Duplicate line under mark."
   (interactive)
   (move-beginning-of-line 1)
   (kill-line)
   (yank)
   (open-line 1)
-  (next-line 1)
+  (forward-line 1)
   (yank))
 
 (provide 'functions)
